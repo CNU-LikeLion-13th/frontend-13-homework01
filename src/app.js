0e59a44339a0875128cb5636b36d1d78bb0c5227
als8921 <als8921@naver.com>
2025-04-07 00:32:04 +0900

    feat : 누른 버튼 스타일 변경, reset 버튼 기능 구현

--- src/app.js
+++ src/app.js
@@ -5,11 +5,30 @@ let totalPrice = 0;
 let tipRatio = 0;
 
 const tipButtons = document.querySelectorAll(".calculator__tip-button");
+const resetButton = document.querySelector(".calculator__reset-button");
+
 const billInput = document.querySelector("#bill");
 const personInput = document.querySelector("#people");
 
 const tipPriceOutput = document.querySelector("#tip_price");
 const totalPriceOutput = document.querySelector("#total_price");
+
+var selectedTipButton = null;
+
+function reset() {
+    personNumber = 1;
+    price = 0;
+    tipPrice = 0;
+    totalPrice = 0;
+    tipRatio = 0;
+
+    billInput.value = "";
+    personInput.value = "";
+    tipPriceOutput.textContent = "$0.00";
+    totalPriceOutput.textContent = "$0.00";
+    if (selectedTipButton != null)
+        selectedTipButton.setAttribute("class", "calculator__tip-button");
+}
 
 function calculatePrice() {
     tipPrice = Number(price * tipRatio);
@@ -20,11 +39,26 @@ function calculatePrice() {
 
 tipButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        tipRatio = Number(button.textContent.replace("%", "") / 100);
+        if (selectedTipButton != null)
+            selectedTipButton.setAttribute("class", "calculator__tip-button");
+
+        if (selectedTipButton === button) {
+            tipRatio = 0;
+            selectedTipButton = null;
+        } else {
+            selectedTipButton = button;
+            selectedTipButton.setAttribute(
+                "class",
+                "calculator__tip-button--selected"
+            );
+            tipRatio = Number(button.textContent.replace("%", "") / 100);
+        }
+
         calculatePrice();
     });
 });
 
+resetButton.addEventListener("click", reset);
 billInput.addEventListener("input", () => {
     price = Number(billInput.value);
     calculatePrice();

cb238c01a3786885f57175a4024ea6ae99c2e74c
JuHeyong <wkdwngud963@gmail.com>
2025-04-08 03:00:10 +0900

    custom function add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const billInput = document.querySelector('input[name="bill"]');
 const peopleInput = document.querySelector('input[name="people"]');
 const tip = document.querySelectorAll('#Select_Tip label');
+const custom_tip_input = document.querySelector('input[name="tip_custom"]');
 const tipOutput = document.querySelector('output[name="tip-result"]');
 const totalOutput = document.querySelector('output[name="total-result"]');
 const reset = document.querySelector('#reset-button');
@@ -10,7 +11,8 @@ let selectedTip = 0;
 tip.forEach((label) => {
     label.addEventListener('click', () => {
         // 모든 라벨에서 selected 제거
-        tip.forEach(l => l.classList.remove('selected'));
+        tip.forEach(label => label.classList.remove('selected'));
+        custom_tip_input.value = '';
 
         // 클릭한 라벨에 selected 추가
         label.classList.add('selected');
@@ -23,13 +25,18 @@ tip.forEach((label) => {
 
 billInput.addEventListener('input', calculate);
 peopleInput.addEventListener('input', calculate);
+custom_tip_input.addEventListener('input', () => {
+    selectedTip = 0;
+    tip.forEach(label => label.classList.remove("selected"));
+    calculate();
+});
 
 function calculate() {
     const bill = parseFloat(billInput.value) || 0;
     const people = parseInt(peopleInput.value) || 1;
 
-    if (bill > 0 && people > 0 && selectedTip > 0) {
-        const tipPerPerson = bill * (selectedTip / 100) / people;
+    if (bill > 0 && people > 0 && (selectedTip > 0 || custom_tip_input.value > 0)) {
+        const tipPerPerson = selectedTip > 0 ? bill * (selectedTip / 100) / people : bill * (custom_tip_input.value / 100) / people;
         const totalPerPerson = bill / people + tipPerPerson;
 
         tipOutput.textContent = "$" + String(Math.floor(tipPerPerson * 100) / 100);
@@ -42,6 +49,7 @@ reset.addEventListener('click', () => {
     peopleInput.value = '';
     tipOutput.value = '';
     totalOutput.value = '';
+    custom_tip_input.value = '';
     selectedTip = 0;
 
     tip.forEach(label => label.classList.remove("selected"))
